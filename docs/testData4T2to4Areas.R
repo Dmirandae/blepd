6603dfae545be7680ac53fe319aa2e6f4ed6802c
Rafael Miranda-Esquivel <dmirandae@gmail.com>
2018-01-17 12:49:05 -0500

    NAMESPACE checked

--- docs/testData4T2to4Areas.R
+++ docs/testData4T2to4Areas.R
@@ -10,27 +10,18 @@ rm(list = ls())
 
 ## libraries
 
-#library(ape)
-#library(vegan)
-#library(picante)
 
 library(blepd)
 
 packageVersion("blepd")
 
 
-library(ggplot2)
-
-library("ggtree")
-
-library(gridExtra)
-
 
 ## directory for test data
 
 rutaactual <- getwd()
 
-setwd("../blepd/testData/")
+setwd("../testData/")
 
 ## data
 
@@ -44,8 +35,6 @@ dist4taxa <- as.matrix(read.table("dist4T00",
  row.names=1)
  )
 
-
-## distributions
 dist4T4A <- as.matrix(read.table("dist4T4A",
  stringsAsFactors=FALSE,
  header=TRUE,
@@ -54,18 +43,15 @@ dist4T4A <- as.matrix(read.table("dist4T4A",
 
 
 
-setwd(rutaactual)
-
-
 ## distribution to XY
 
 distXY <- matrix2XY(dist4taxa)
 
 
 
-## recalculate length brnach terminal A
+## recalculate length branch terminal A
 
-## change the brnach value in A
+## change the branch value in A
 ## case 2 loger than the remaining branches
 
 tipNumber <- which(initialTree$tip.label == "t1")
@@ -107,20 +93,8 @@ print(eval)
 
 PDvalue <- myPD(tree = initialTree, distribution = dist4taxa)
 
-p0 <-    ggtree(modifiedTree, layout="slanted", ladderize=TRUE,
-                       color="black", size=1 
-                       ) + 
-              #geom_tiplab(size=6, aes(color=c(as.character(eval$areaDelta)))) +
-              #geom_tiplab(aes(color=c(as.character(eval$areaDelta)))) +
-               #theme_tree2() +  
-               theme(legend.position="right") +
-                labs(title = paste("t1:",valor1,"t3:",valor2,"Four terminals, non-equal branch length; PD",PDvalue[1],PDvalue[2]))
+PDvalue
 
-#p0
-
-p <- p0 %<+% eval + geom_tiplab(aes(color=paste(areaDelta,abDelta,sep="_")), size =6) 
-
-print(p)
 
 }}
 
