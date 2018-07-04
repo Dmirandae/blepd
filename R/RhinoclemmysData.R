7677983f114cf54d96a395524f8fc01b0aadc33e
Rafael Miranda-Esquivel <dmirandae@gmail.com>
2018-07-03 22:03:13 -0500

    minor changes

--- R/RhinoclemmysData.R
+++ R/RhinoclemmysData.R
@@ -1,13 +1,26 @@
 #'
 #' @title
-#' A tree and distribution for Rhinoclemmys 
+#' Trees and distributions for Rhinoclemmys, an example data analysis 
+#' 
 #' 
 #' @description
-#' A tree as an APE object with four terminals.
-#'
+#' Three trees as an APE object with distributions, and analysis:
+#' igrClock_exp_genus     :: iGr clock, exponential, genera constrained
+#' igrClock_exp_uniform   :: iGr clock, exponential, genera un constrained
+#' NonClock_genus         ::  No clock, genera constrained
 #' 
-#' @format An APE object, class phylo. A tree dataset with four terminals.
-#'
+#' 
+#' @format A list with three phylo objects, and the resulting analyses: 
+#' RhinoclemmysData$distribution :: a matrix object
+#' RhinoclemmysData$trees  :: phylo objects
+#' RhinoclemmysData$tablePD :: a matrix object
+#' RhinoclemmysData$matrixPD  :: a matrix object
+#' RhinoclemmysData$nameTrees :: a vector of chars
+#' RhinoclemmysData$nameAreas :: a vector of chars
+#' RhinoclemmysData$nameTerminals  :: a vector of chars
+#' RhinoclemmysData$evalPD  :: a vector of reals
+#' RhinoclemmysData$sumDeltas  :: a vector of reals
+#' RhinoclemmysData$maxPD  :: a vector of reals
 #'
 #'
 #'@author Miranda-Esquivel Daniel R.
